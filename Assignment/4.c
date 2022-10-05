c2e7744bd3959cdbba3818145400f019ee262a2e
Md Musabbir <103814849+musabbir18@users.noreply.github.com>
2022-10-05 21:45:00 +0530

    its my test assignment file

---
+++ Assignment/4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    float r;
+    printf("enter the radius;");
+    scanf("%f",&r);
+    float area=3.14*r*r;
+    printf("area is %f",area);
+     return 0;
+
+}
